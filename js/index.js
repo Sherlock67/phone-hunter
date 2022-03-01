9ae90eb2ee99e448088274962cd8f579f16f6b25
Sherlock67 <alammushfiqul366@gmail.com>
2022-03-02 00:01:59 +0600

    needed fine tuning

--- js/index.js
+++ js/index.js
@@ -1,16 +1,19 @@
 
 const searchPhone = () =>{
+  const nothingFound = document.getElementById('nothing-found');
     const searchField = document.getElementById('search-field')
     const blankSearch = document.getElementById('blank-search');
     const searchText = searchField.value;
     searchField.value = '';
     blankSearch.textContent = '';
-    if(searchText == ''){
+    
+    if(searchText === ''){
       const paragraph = document.createElement('p');
       paragraph.classList.add('text-danger');
       paragraph.innerHTML = `Please enter a String value`;
       blankSearch.appendChild(paragraph);
-      
+      blankSearch.style.display = 'block'
+      nothingFound.style.display = 'none';
     }else{
       const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
       fetch(url)
@@ -21,17 +24,19 @@ const searchPhone = () =>{
 
 
 const displayResult = (result) =>{
-  
+ 
   const {data, status} = result;
   const nothingFound = document.getElementById('nothing-found');
   const searchResult = document.getElementById('search-result');
+  const blankSearch = document.getElementById('blank-search');
   searchResult.textContent = '';
   nothingFound.textContent = '';
    if(status === false){
      var paragraph = document.createElement('p');
      paragraph.innerHTML=`No Phone Found`;
      nothingFound.appendChild(paragraph);
-    
+     blankSearch.style.display = 'none';
+     nothingFound.style.display = 'block';
    } 
   data.forEach(phone => {
     console.log(phone);
@@ -58,13 +63,18 @@ const loadPhoneDetails = phoneId =>{
   .then(data =>showPhoneDetails(data.data))
 }
 const showPhoneDetails = data => {
+  const searchResult = document.getElementById('search-result');
   const phoneDetails = document.getElementById('phone-details')
   phoneDetails.textContent = '';
+  
   const div = document.createElement('div');
   div.classList.add('card');
   div.innerHTML =`
-  <img src="${data.image}" class="card-img-top img-fluid" alt="...">
+  <img src="${data.image}" class="card-img-top" alt="...">
   <div class="card-body >
+    
+  
+
     <h5 class="card-title">${data.releaseDate}</h5>
     <h5>Main Features</h5>
     <ul class="list-group">
@@ -84,12 +94,12 @@ const showPhoneDetails = data => {
     </ul>
     <h5>Other Features</h5>
     <ul class="list-group">
-    <li class="list-group-item">${data.mainFeatures.others.Bluetooth}</li>
-    <li class="list-group-item">${data.mainFeatures.GPS}</li>
-    <li class="list-group-item">${data.mainFeatures.NFC}</li>
-    <li class="list-group-item">${data.mainFeatures.Radio}</li>
-    <li class="list-group-item">${data.mainFeatures.USB}</li>
-    <li class="list-group-item">${data.mainFeatures.WLAN}</li>
+  
+    <li class="list-group-item">${data.others.GPS}</li>
+    <li class="list-group-item">${data.others.NFC}</li>
+    <li class="list-group-item">${data.others.Radio}</li>
+    <li class="list-group-item">${data.others.USB}</li>
+    <li class="list-group-item">${data.others.WLAN}</li>
     </ul>
   </div>`
   ;
